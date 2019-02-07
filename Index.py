068f833e092f11e73d59d71480f73ffeaad03eb8
bodovix <stbbap@gmail.com>
2019-02-07 20:14:26 +0000

    pre processing class setup

--- Index.py
+++ Index.py
@@ -2,22 +2,22 @@ import cv2
 from matplotlib import pyplot as plt
 
 from skimage import io, color
-img = io.imread('baboon.png')
+img = io.imread('Images/baboon.png')
 dimensions = color.guess_spatial_dimensions(img)
 print(dimensions)
 
 # get  shape
 import skimage.io as io
 from skimage.color import rgb2gray
-img = io.imread('baboon.png')
+img = io.imread('Images/baboon.png')
 print (img.shape)
 
 # create grey scale
 import skimage.io as io
 from skimage.color import rgb2gray
-img = io.imread('baboon.png')
+img = io.imread('Images/baboon.png')
 img_grayscale = rgb2gray(img)
-io.imsave('baboon-gs.png', img_grayscale)
+io.imsave('Images/baboon-gs.png', img_grayscale)
 
 # view image
 show_grayscale = io.imshow(img_grayscale)
@@ -29,9 +29,9 @@ io.show()
 
 #opencv
 # apply bilateral filter
-img = cv2.imread('baboon-gs.png')
+img = cv2.imread('Images/baboon-gs.png')
 blur = cv2.bilateralFilter(img, 9, 75, 75)
-cv2.imwrite("bilateralFilter.png", blur)
+cv2.imwrite("Images/bilateralFilter.png", blur)
 
 # display image
 plt.subplot(121), plt.imshow(img), plt.title('Original')
--- Run.py
+++ Run.py
@@ -1,16 +1,21 @@
 import cv2
+from PreProcessing import PreProcessor
 
-import BilateralFilter
+import PreProcessing
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('baboon.png')
+img = cv2.imread('Images/face-2.jpg')
+preProcessor = PreProcessor()
 
-
+greyScaleImg = preProcessor.applygreyscale(img)
+bFilter = preProcessor.applybilateralfilter(greyScaleImg)
+print("run ran")
 
 #cv2.imwrite("bilateralFilter.png", bFilter)
 
 #display image
+
 plt.subplot(122), plt.imshow(bFilter), plt.title('Blurred')
 plt.xticks([]), plt.yticks([])
 plt.show()
